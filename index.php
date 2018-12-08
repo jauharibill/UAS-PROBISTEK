d1f2ad0dd9b2132493366f14eb3983c6789be2ac
seviansyah <seviansyahf@gmail.com>
2018-12-09 02:51:37 +0700

    ngasih bachground aja kali aja cocok
    nambahin stayle.css
    sama isi tampilan kali aja cocok

--- index.php
+++ index.php
@@ -8,48 +8,18 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
     <title>PROBISTEK</title>
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" href="images/UIN.png">
+
     <script type="text/javascript" src="jquery-3.3.1.js"></script>
     <script type="text/javascript" src="js/bootstrap.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
     <style>
-        body {
-            font-family: helvetica;
-        }
-        .image {
-            padding-top: 25px;
-            padding-bottom: 25px;
-        }
-        .col-6.contact {
-            padding-top: 40px;            
-            margin-bottom: 0%;
-        }
-        #contact1 {
-            color: #58585A;
-            text-align: right;
-            font-size: 21px;
-            margin-bottom: 0px;
-        }   
-        table.contact2 {
-            float: right;
-            margin-top: 0%;
-            padding-right: 0px;
-            color: #808080;
-        }
-        .navbar {
-            background-color: #0093dd !important;
-            font-size: 17px;
-            font-weight: bold;
-            color: #0093dd;
-        }
-        .navbar-nav {
-            padding-left: 110px;
-        }
-        #adminBtn {
-            margin-right: 110px;
-        }
+       
     </style>
 </head>
 
@@ -72,7 +42,7 @@
             </div>
         </div>
         <div>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <nav class="navbar navbar-expand-lg navbar-light bg-primary">
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                     aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -91,6 +61,71 @@
             </nav>
         </div>
     </header>
+<!-- Halaman Index -->
+
+    <div class="pimg1">
+        <div class="ptext">
+            <span class="border">
+                HALAMAN INDEX
+            </span>
+        </div>
+            <p class="tes">INI HANYA CONTOH</p>
+    </div>
+    
+
+    <section class="section section-dark">
+        <h2>DOSEN</h2>
+            <div class="row">
+              <div class="col-md-4 col- ">
+                  <img class="img-fluid" 
+                      src="images/masayom.jpg" 
+                      style="height: 150px;">
+                  <p>Ayyom Muharrom</p>
+              </div>
+              <div class="col-md-4 col-sm-6 ">
+                  <img class="img-fluid" 
+                       src="images/masbill.jpg" 
+                       style="height: 150px;">
+                  <p>Bill Tanthowi Jauhari</p>  
+              </div>
+              <div class="col-md-4 col-sm-6 ">
+                  <img class="img-fluid"
+                       src="images/mbkeni.jpg" 
+                       style="height: 150px;">  
+                  <p>Nur Aini</p>
+            </div>
+    </section>
+    <div class="pimg2">
+        <div class="ptext">
+            <span class="border trans">
+                PROBISTEK
+            </span>
+        </div>
+    </div>
+
+
+    <section class="section section-dark">
+        <h2>Title Second Text </h2>
+        <p>
+            Lorem Ipsum adalah contoh teks atau dummy dalam industri percetakan dan penataan huruf atau typesetting. Lorem Ipsum telah menjadi standar contoh teks sejak tahun 1500an, saat seorang tukang cetak yang tidak dikenal mengambil sebuah kumpulan teks dan mengacaknya untuk menjadi sebuah buku contoh huruf. Ia tidak hanya bertahan selama 5 abad, tapi juga telah beralih ke penataan huruf elektronik, tanpa ada perubahan apapun. Ia mulai dipopulerkan pada tahun 1960 dengan diluncurkannya lembaran-lembaran Letraset yang menggunakan kalimat-kalimat dari Lorem Ipsum, dan seiring munculnya perangkat lunak Desktop Publishing seperti Aldus PageMaker juga memiliki versi Lorem Ipsum.
+            Mengapa kita menggunakannya?
+        </p>
+    </section>
+    <div class="pimg3">
+        <div class="ptext">
+            <span class="border trans">
+                UAS RPOBISTEK
+            </span>
+        </div>
+    </div>
+
+    <section class="section section-dark">
+        <h2>Title Third Text </h2>
+        <p>
+            Lorem Ipsum adalah contoh teks atau dummy dalam industri percetakan dan penataan huruf atau typesetting. Lorem Ipsum telah menjadi standar contoh teks sejak tahun 1500an, saat seorang tukang cetak yang tidak dikenal mengambil sebuah kumpulan teks dan mengacaknya untuk menjadi sebuah buku contoh huruf. Ia tidak hanya bertahan selama 5 abad, tapi juga telah beralih ke penataan huruf elektronik, tanpa ada perubahan apapun. Ia mulai dipopulerkan pada tahun 1960 dengan diluncurkannya lembaran-lembaran Letraset yang menggunakan kalimat-kalimat dari Lorem Ipsum, dan seiring munculnya perangkat lunak Desktop Publishing seperti Aldus PageMaker juga memiliki versi Lorem Ipsum.
+            Mengapa kita menggunakannya?
+        </p>
+    </section>
 
     <br><br><br><br><br>
 
