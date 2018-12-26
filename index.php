acd094cc4d781812eaac087c5a26a0461d3a330a
seviansyah <seviansyahf@gmail.com>
2018-12-26 13:48:11 +0700

    ada troubel
    jadi saat memindahkan css ke project uas, ada yang gak mau sinkron padahal sudah di panggil  scrip dan link css nya....

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
     <link rel="icon" href="images/UIN.png">
 
 
+
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"> 
@@ -22,13 +23,12 @@
     <script type="text/javascript" src="jquery-3.3.1.js"></script>
     <script type="text/javascript" src="js/bootstrap.js"></script>
     
-    
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 
     <!--link icon-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
 
+   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"> 
    <link rel="stylesheet" type="text/css" href="css/bootstrap.css"> 
     <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
